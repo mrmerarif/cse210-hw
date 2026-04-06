f9da11b900896a186ecc6004c0a275dc0bb1796b
mrmerarif <bflores2@byupathway.edu>
2026-04-05 22:22:53 -0600

    Activity 1

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test the base class
+        Assignment a1 = new Assignment("Hugo Sanchez", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+        // Test the MathAssignment class
+        MathAssignment m1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+        Console.WriteLine();
+
+        // Test the WritingAssignment class
+        WritingAssignment w1 = new WritingAssignment("Mary Hernandez", "European History", "The French Revolution");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
